b94e5a8d8590f752de92867f9bc03725cd072792
Olena Abert <oabert@gmail.com>
2024-02-28 10:39:02 -0500

    add db configuration

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,6 @@
 """
-Django commands to wait for the db to be available
+Django command to wait for the database to be available.
 """
-
 import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
@@ -26,4 +25,3 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
