f61bd93b5b31acebcce7a51456fa6281a196b5ae
Olena Abert <oabert@gmail.com>
2024-03-01 11:30:17 -0500

    first migratin_3

--- app/core/models.py
+++ app/core/models.py
@@ -1,10 +1,10 @@
 """
 Database models.
 """
-import uuid
-import os
+# import uuid
+# import os
 
-from django.conf import settings
+# from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -18,9 +18,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        if not email:
-            raise ValueError('User must have an email address.')
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
